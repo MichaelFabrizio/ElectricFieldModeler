0ffb09e69d3a454d8a28022b212d4dca115e1ddb
MichaelFabrizio <mikefabrizio94@gmail.com>
2021-07-17 10:14:54 -0400

    Updated ignore config and premake
    Should now ignore external project files and properly compile.

--- premake5.lua
+++ premake5.lua
@@ -1,9 +1,9 @@
 -- premake5.lua
-workspace "Game"
+workspace "ElectricFieldModeler"
     architecture "x64"
     configurations { "Debug", "Release", cppdialect "C++17" }
 
-project "Game"
+project "ElectricFieldModeler"
     kind "ConsoleApp"
     language "C++"
     targetdir "bin/%{cfg.buildcfg}"
@@ -13,12 +13,12 @@ project "Game"
     "Game/src/**.cpp",
     "Game/include/**.h",
 
-    "../extern/lib/Glad/glad.c",
-    "../extern/include/stb/**.h",
-    "../extern/include/imgui-master/*.h",
-    "../extern/include/imgui-master/*.cpp",
-    "../extern/include/imgui-master/backends/imgui_impl_opengl3.cpp",
-    "../extern/include/imgui-master/backends/imgui_impl_sdl.cpp"
+    "extern/lib/Glad/glad.c",
+    "extern/include/stb/**.h",
+    "extern/include/imgui-master/*.h",
+    "extern/include/imgui-master/*.cpp",
+    "extern/include/imgui-master/backends/imgui_impl_opengl3.cpp",
+    "extern/include/imgui-master/backends/imgui_impl_sdl.cpp"
     }
 
     links
@@ -32,21 +32,21 @@ project "Game"
 
    	includedirs
     {
-        "../extern/include/SDL",
-        "../extern/include/Glad",
-        "../extern/include/Glad/**",
-        "../extern/include/stb",
-        "../extern/include/entt",
-        "../extern/include/imgui-master/",
-        "../extern/include/imgui-master/backends"
+        "extern/include/SDL",
+        "extern/include/Glad",
+        "extern/include/Glad/**",
+        "extern/include/stb",
+        "extern/include/entt",
+        "extern/include/imgui-master/",
+        "extern/include/imgui-master/backends"
     }
 
  	libdirs
     {
-        "../extern/lib/SDL",
-        "../extern/lib/Glad",
-        "../extern/include/imgui-master/",
-        "../extern/include/imgui-master/backends"
+        "extern/lib/SDL",
+        "extern/lib/Glad",
+        "extern/include/imgui-master/",
+        "extern/include/imgui-master/backends"
     }
 
     filter "configurations:Debug"
